748382f300a940872800ee1940fc1b9555b41d1f
xVesper <73543770+xVesper@users.noreply.github.com>
2024-12-03 11:51:32 +0100

    Update ssi.py

--- ssi.py
+++ ssi.py
@@ -1,11 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
-from math import pi
 import numpy as np
-
-from pandas.errors import EmptyDataError
-
 
 def data_load():
     try:
@@ -28,7 +24,7 @@ def data_load():
 
     return df
 
-#print(data_load())
+# data_load()
 
 def draw_smile():
 
@@ -50,7 +46,7 @@ def draw_smile():
     ax.legend(loc='center right', bbox_to_anchor=(1.5, 0.5))
     plt.show()
 
-#draw_smile()
+# draw_smile()
 
 def list_classes(df):
     classes_col = df['class']
@@ -91,8 +87,83 @@ for i, clas in enumerate(unique_classes):
     axs[1, 0].set_ylabel('petal_width_in_cm')
     axs[1, 0].legend()
     axs[1, 1].scatter(x4[classes == clas], y4[classes == clas], label=names[i], color=colors[i])
-    axs[1, 1].set_xlabel('petal_length_in_cm')
-    axs[1, 1].set_ylabel('petal_width_in_cm')
+    axs[1, 1].set_xlabel('petal_width_in_cm')
+    axs[1, 1].set_ylabel('petal_length_in_cm')
     axs[1, 1].legend()
 
-plt.show()
+# plt.show()
+
+def k_sre():
+
+    data = np.loadtxt('spirala.txt')
+    n = 4
+    iter = 100
+
+    middle = data[np.random.choice(data.shape[0], n, replace=False)]
+
+    for _ in range(iter):
+        distances = np.zeros((data.shape[0], n))
+
+        for i in range(data.shape[0]):
+            for k in range(n):
+                distances[i, k] = np.sqrt(np.sum((data[i] - middle[k]) ** 2))
+        labels = np.argmin(distances, axis=1)
+
+        new_middle = np.array([data[labels == i].mean(axis=0) for i in range(n)])
+        if np.all(middle == new_middle):
+            break
+
+        middle = new_middle
+
+    plt.figure(figsize=(8, 6))
+    for i in range(n):
+        plt.scatter(data[labels == i][:, 0], data[labels == i][:, 1], s=20, alpha=.70, label=f'Klaster {i + 1}')
+    plt.scatter(new_middle[:, 0], new_middle[:, 1], color='red', s=100, marker='x', label='Środki', linewidths=2)
+    plt.title("K-Średnich")
+    plt.legend()
+    plt.show()
+
+#k_sre()
+
+def algorithm():
+
+    zakres_zmiennosci = [0, 100]
+
+    x_plot = np.linspace(zakres_zmiennosci[0], zakres_zmiennosci[1], 1000)
+    y_plot = np.sin(x_plot/10)*np.sin(x_plot/200)
+
+    x = np.random.uniform(zakres_zmiennosci[0], zakres_zmiennosci[1])
+    y = np.sin(x / 10) * np.sin(x / 200)
+
+    rozrzut = 10
+    wsp_przyrostu = 1.1
+    iteracja = 100
+
+    for _ in range(iteracja):
+        x_pot = x + np.random.uniform(-rozrzut, rozrzut)
+
+        if x_pot > zakres_zmiennosci[1]:
+            x_pot = zakres_zmiennosci[1]
+        elif x_pot < zakres_zmiennosci[0]:
+            x_pot = zakres_zmiennosci[0]
+
+        y_pot = np.sin(x_pot / 10) * np.sin(x_pot / 200)
+
+        if y_pot >= y:
+            x = x_pot
+            y = y_pot
+            rozrzut *= wsp_przyrostu
+        else:
+            rozrzut /= wsp_przyrostu
+        plt.scatter(x, y, color='k', label=f'({x:.2f}, {y:.2f})')
+        print('nr iteracji: ', _, ": ", x,y, rozrzut)
+
+    plt.plot(x_plot, y_plot, linestyle='-', color='k', label='Funkcja')
+    plt.scatter(x, y, color='r', label=f'({x:.2f}, {y:.2f})', marker=".", linewidths=5)
+    plt.title("Algorytm 1 + 1")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.grid(True)
+    plt.show()
+
+#algorithm()
